0cf8d9fb645c697764b99775ed5ac230de311caf
Rich Mangan <riccimangan@gmail.com>
2016-02-01 21:51:16 +0000

    Car Garage ArrayList example

---
+++ motorcarsgarage/Cars.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package motorcarsgarage;
+
+/**
+ * @author Rich
+ */
+public class Cars {
+    String make, model;
+    int year;
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+    
+    
+    
+}
---
+++ motorcarsgarage/MotorCarsGarage.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package motorcarsgarage;
+
+/**
+ *
+ * @author Rich
+ */
+public class MotorCarsGarage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        GUICarsApp GUI = new GUICarsApp();
+        GUI.setVisible(true);
+    }
+    
+}
